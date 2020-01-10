6d0f648f70c822f975330be9f51c0e5a92f3d53d
Mike Dmitriev <misha.dmitriev.spb@gmail.com>
2020-01-10 18:35:11 +0300

    I Am Rich

---
+++ Training Projects/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Mike on 09.01.2020.
+//  Copyright © 2020 Mike Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
