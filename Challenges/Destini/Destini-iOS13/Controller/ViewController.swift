d2d360d60c9d0fe9cc5c3ba322b60d2c97f8c462
Mike Dmitriev <misha.dmitriev.spb@gmail.com>
2020-01-12 17:41:34 +0300

    Challenge. Destini MVC Pattern

---
+++ Challenges/Destini/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//-----------------------------------------------------------------------------
+//LINKS
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+//-----------------------------------------------------------------------------
+//VARIABLES
+    var storyBrain = StoryBrain()
+    
+//-----------------------------------------------------------------------------
+//FIRST LOAD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+//-----------------------------------------------------------------------------
+//BUTTON PRESSED
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+    }
+    
+    //UPDATE UI
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+}
+
+
+
+
+
+
+
+
